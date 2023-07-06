f70a076103c7888ce6078bf21c241cb8bec82f10
Kaushal99999 <96714323+Kaushal99999@users.noreply.github.com>
2023-07-06 22:20:42 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -249,46 +249,50 @@ async function initMap() {
       ], gestureHandling: "greedy"
   });
 
-  const buttons = [
-    ["Rotate Left", "rotate", 20, google.maps.ControlPosition.LEFT_CENTER],
-    ["Rotate Right", "rotate", -20, google.maps.ControlPosition.RIGHT_CENTER],
-    ["Tilt Down", "tilt", 20, google.maps.ControlPosition.TOP_CENTER],
-    ["Tilt Up", "tilt", -20, google.maps.ControlPosition.BOTTOM_CENTER],
-  ];
-
-
-
-   buttons.forEach(([text, mode, amount, position]) => {
-    const controlDiv = document.createElement("div");
-    const controlUI = document.createElement("button");
-
-    controlUI.classList.add("ui-button");
-    controlUI.innerText = `${text}`;
-    controlUI.addEventListener("click", () => {
-      adjustMap(mode, amount);
-    });
-    controlDiv.appendChild(controlUI);
-    map.controls[position].push(controlDiv);
-  });
-
-  const adjustMap = function (mode, amount) {
-    switch (mode) {
-      case "tilt":
-        map.setTilt(map.getTilt() + amount);
-        break;
-      case "rotate":
-        map.setHeading(map.getHeading() + amount);
-        break;
-      default:
-        break;
-    }
-  };
-  
-
-
 
     
 }
+
+
+const rotateLeftButton = document.getElementById("rotateLeftButton");
+const rotateRightButton = document.getElementById("rotateRightButton");
+const tiltDownButton = document.getElementById("tiltDownButton");
+const tiltUpButton = document.getElementById("tiltUpButton");
+
+// Attach event listeners to the buttons
+rotateLeftButton.addEventListener("click", () => {
+  adjustMap("rotate", 20);
+});
+
+rotateRightButton.addEventListener("click", () => {
+  adjustMap("rotate", -20);
+});
+
+tiltDownButton.addEventListener("click", () => {
+  adjustMap("tilt", 20);
+});
+
+tiltUpButton.addEventListener("click", () => {
+  adjustMap("tilt", -20);
+});
+
+const adjustMap = function (mode, amount) {
+  switch (mode) {
+    case "tilt":
+      map.setTilt(map.getTilt() + amount);
+      break;
+    case "rotate":
+      map.setHeading(map.getHeading() + amount);
+      break;
+    default:
+      break;
+  }
+};
+
+
+
+
+
 
 
 
